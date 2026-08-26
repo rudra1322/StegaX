15eab2de07883345148a20505332297a23559db6
blackmafia-source <0xblackmafia@gmail.com>
2026-08-26 23:46:46 +0530

    Update StegaX metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { Suspense } from "react"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "StegaX",
+  description: "Created with BlackMafia",
+  generator: "blackmafia.in",
 }
 
 export default function RootLayout({
